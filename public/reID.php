a7a028cee3515719b7e772e12f24f51805bc5b03
and703 <andwishyou@gmail.com>
2026-06-24 09:31:26 +0700

    Update reID.php query and status handling
    Change database query to use mman table and Paint_id column; simplify row handling to always display the "Continue ReBoiacca" message for matched records and add an "Invalid ID" message when no rows are returned. Removes the previous conditional that checked M_id == '0'.

--- public/reID.php
+++ public/reID.php
@@ -6,7 +6,7 @@
 	}
 	
 	$no = 1;
-	$query = "SELECT * FROM painting WHERE id =?";
+	$query = "SELECT * FROM mman WHERE Paint_id =?";
 	$dewan1 = $db1->prepare($query);
 	$dewan1->bind_param('s', $s_keyword);
 	$dewan1->execute();
@@ -14,9 +14,9 @@
 
 	if ($res1->num_rows > 0) {
 		while ($row = $res1->fetch_assoc()) {
-			if($row['M_id'] == '0'){
-				echo '<h5 class="card-title" style="font-size: 400%; color: #FFF017;">Inside Process</h4>';
-			}
+			echo '<h5 class="card-title" style="font-size: 400%; color: #FFF017;">Continue ReBoiacca</h4>';
 		} 
+	}else {
+		echo '<h5 class="card-title" style="font-size: 400%; color: #FFF017;">Invalid ID</h4>';
 	}
 ?>
